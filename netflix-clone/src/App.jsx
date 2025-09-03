fee7d720b5bd06efa44b951e6d1649ff37c2d2f2
biru-ka2 <bworku1221@gmail.com>
2025-09-03 14:39:29 +0300

    header and footer and also Home page create

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Home from "./page/Home/Home";
 
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
